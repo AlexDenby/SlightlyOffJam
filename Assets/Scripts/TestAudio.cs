ee9ebf8f5446862bf664dc2df656fdb1b82f2c41
Casey Rounseville <cas2136714@maricopa.edu>
2018-10-27 15:09:17 -0700

    Add Audio Trigger

---
+++ Assets/Scripts/TestAudio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAudio : MonoBehaviour {
+	private AudioSource src;
+
+	void Start() {
+		src = GetComponent<AudioSource>();
+	}
+
+	public void playSrc() {
+		src.Play();
+	}
+}
